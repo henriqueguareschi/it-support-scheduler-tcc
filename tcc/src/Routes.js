4a321022df22bb7f32ac3b3cf0ebf1a1e237af2a
henriqueguareschi <81583752+henriqueguareschi@users.noreply.github.com>
2022-04-05 20:39:25 -0300

    2

--- tcc/src/Routes.js
+++ tcc/src/Routes.js
@@ -3,14 +3,20 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-
 } from "react-router-dom";
 import Login from "./Components/Login";
+import Home from "./Pages/Home";
 
 const Routes = () => {
     return (
         <Router>
             <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/home">
+                    <Home />
+                </Route>
                 <Route exact path="/login">
                     <Login />
                 </Route>
